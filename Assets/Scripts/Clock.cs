d4c047497377b227c3e62c9821d3fdc55c5bbf54
Lydia Symchych <94412624+lydia-symchych@users.noreply.github.com>
2024-01-10 11:11:41 -0600

    I made a windmill clock that rotates for 360 frames, not 360 seconds. Help plz

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    public bool clockIsRunning;
     public float DayLength;
-    private float secondsToDegrees;
+    private float degreesRotation;
+    private float timeLeft;
 
     public Transform handPos;
-
-    public float startPos;
-    public float endPos;
 
     // Start is called before the first frame update
     void Start()
     {
+        //get handPos rotation in z
         //get DayLength
-        //360 / DayLength = degreesRotation per frame
-            // if we want a 9-5 we should move the hand to start at 9 and instead of 360 use 240
+
+        degreesRotation = 360f / DayLength;
+        // if we want a 9-5 we should move the hand to start at 9 and instead of 360 use 240
+        timeLeft = 360f;
         //timeLeft = 360 which is assuming that one round of the timer is a full rotation from 12 to 12
             // timeLeft = 240 if we have an 8 hours shift
     }
@@ -26,13 +26,21 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if timeLeft > 0 
-        //move hand degreesRotation
-            // which means hand 0,0,Z rotation - 0,0,degreesRotation
-        //timeLeft - degreesRotation
-       
-        //else if timeLeft = 0
-        //stopHand
+        if (timeLeft > 0f)
+        {
+            //move hand degreesRotation
+            handPos.eulerAngles += new Vector3(0, 0, timeLeft - degreesRotation);
+            //set new timeLeft
+            timeLeft = timeLeft - degreesRotation;
+            Debug.Log(timeLeft);
+        }
+
+        else if (timeLeft == 0f)
+        {
+            //do nothing
+            Debug.Log("Time's up");
+        }
+
     }
 
 }
