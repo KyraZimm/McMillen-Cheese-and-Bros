b9b4b8995f596b9051aac7f3950ca70ee96fb1c5
Lydia Symchych <94412624+lydia-symchych@users.noreply.github.com>
2024-01-10 10:41:52 -0600

    Added some notes on how to change code for an 8 hour shift

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -18,9 +18,9 @@ public class Clock : MonoBehaviour
     {
         //get DayLength
         //360 / DayLength = degreesRotation per frame
+            // if we want a 9-5 we should move the hand to start at 9 and instead of 360 use 240
         //timeLeft = 360 which is assuming that one round of the timer is a full rotation from 12 to 12
-
-        
+            // timeLeft = 240 if we have an 8 hours shift
     }
 
     // Update is called once per frame
@@ -28,6 +28,7 @@ public class Clock : MonoBehaviour
     {
         //if timeLeft > 0 
         //move hand degreesRotation
+            // which means hand 0,0,Z rotation - 0,0,degreesRotation
         //timeLeft - degreesRotation
        
         //else if timeLeft = 0
