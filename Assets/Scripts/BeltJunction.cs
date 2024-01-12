e732bf850c594504453bb474472375ae92e5d1f0
Kyra <kyra@notsuspicious.biz>
2024-01-12 13:20:38 -0800

    tag comparison example

--- Assets/Scripts/BeltJunction.cs
+++ Assets/Scripts/BeltJunction.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 
 public class BeltJunction : MonoBehaviour {
 
+    private void OnTriggerEnter2D(Collider2D collision) {
+        BeltItem item = collision.GetComponent<BeltItem>();
+
+        if (item == null)
+            return;
+
+        if (item is Cheese) {
+            //if good, do thing
+            //if bad, do thing
+        }
+        else {
+
+        }
+        
+
+    }
 }
