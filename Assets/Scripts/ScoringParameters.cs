6c8a185f7104f092491681ea7425460768b3947f
Kyra <kyra@notsuspicious.biz>
2024-01-18 13:17:24 -0800

    comments on score parameters

--- Assets/Scripts/ScoringParameters.cs
+++ Assets/Scripts/ScoringParameters.cs
@@ -30,4 +30,30 @@ public class ScoringParameters : ScriptableObject
     public float recyclingInCompost;
     public float compostInRecycling;
     public float compostInCompost;
+
+
+    ///SUGGESTIONS FROM KYRA
+    //instead of making a separate float for each possible situation we could have for scoring, we can create a searchable dictionary or list for our scoring paramters.
+    //The goal is to make a function where we just have to plug in the item type we have and the item type we need, and get a score for that item as a result, instead of doing a bunch of manual work to compare items.
+    //Otherwise, if we have a float for each possible scenario, and each float has a different name, it gets really hard to scale and reconfigure things.
+
+    //So for example, I'll make a new scoring struct:
+    [System.Serializable] public struct ScoreScenario {
+        public ItemTag itemInput;
+        public bool inputIsGood; //for if you're checking a good/bad cheese
+        public ItemTag desiredItem;
+        public bool desiredInputIsGood; //for if the bin specifically needs good/bad cheese
+        public float score;
+    }
+
+    //and then add a section to the inspector where the designer can plug in score values:
+    public List<ScoreScenario> scoreScenarios;
+    //this means you can put a bunch of stuff in the inspector that says "If the player gives you a cheddar, but you wanted a recycling item, you give them -1 points."
+    //"If the player gives you a bad cheddar, but you needed a good cheddar, give them -2 points."
+    //etc
+
+    //then, in ScoreKeeper, we can write a function with some parameters:
+    private void ModifyScore(BeltItem item, ItemTag desiredItem) {
+        //and then inside this function, we can search our ScoreScenario inputs to find the condition that matches our input item and desired item, and return the score value we want.
+    }
 }
