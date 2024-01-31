bf5d3aed721fd25e72333b271fb77f0335219789
Kyra <kyra@notsuspicious.biz>
2024-01-31 12:21:38 -0800

    items missed in last commit

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -8,6 +8,7 @@ public class ConveyorBelt : MonoBehaviour {
     [SerializeField] string[] itemsToSpawn;
     [SerializeField] float spawnInterval;
     [SerializeField] Transform spawnPoint;*/
+    [SerializeField] DaySettings spawnSettings;
 
     public Vector2 BeltVector { get; private set; }
     public Vector2 BeltVectorNormalized { get; private set; }
@@ -36,7 +37,7 @@ public class ConveyorBelt : MonoBehaviour {
             item.MoveToPos(newTargetPos);
         }
 
-        if (Time.time - timeLastItemSpawned >= DaySettings.Instance.SpawnBehaviour.SpawnInterval) {
+        if (Time.time - timeLastItemSpawned >= spawnSettings.SpawnInterval) {
             SpawnItem();
             timeLastItemSpawned = Time.time;
         }
@@ -44,13 +45,13 @@ public class ConveyorBelt : MonoBehaviour {
 
     private void SpawnItem() {
         //get random item to spawn
-        string newItemToSpawn = DaySettings.Instance.SpawnBehaviour.GetRandomItem().ItemName;
+        string newItemToSpawn = spawnSettings.GetRandomItem();
         BeltItem newItem = Instantiate(ItemReference.Instance.ItemPrefabs[newItemToSpawn], startPoint, Quaternion.identity).GetComponent<BeltItem>();
 
         //if item is cheese, determine quality
         if (newItem is Cheese) {
             Cheese newCheese = newItem as Cheese;
-            newCheese.SetQuality(Random.Range(0f, 1f) >= DaySettings.Instance.SpawnBehaviour.ChanceOfBadCheese);
+            newCheese.SetQuality(Random.Range(0f, 1f) >= spawnSettings.ChanceOfBadCheese);
         }
 
         //initialize new item
---
+++ Assets/Scripts/DaySettings.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item Spawn Settings", menuName = "ScriptableObjects/Item Spawn Settings")]
+public class DaySettings : ScriptableObject {
+    public float SpawnInterval;
+    public float ChanceOfBadCheese;
+    public SpawnItem[] ItemsToSpawn;
+
+    private float cumulativeSpawnChance = 0f;
+    private float[] spawnChancesPerItem;
+    
+    [System.Serializable] public struct SpawnItem {
+        public string Name;
+        public float ChanceOfSpawningModifier;
+    }
+
+    public void Awake() {
+        spawnChancesPerItem = new float[ItemsToSpawn.Length];
+        for (int i = 0; i < ItemsToSpawn.Length; i++) {
+            cumulativeSpawnChance += ItemsToSpawn[i].ChanceOfSpawningModifier;
+            spawnChancesPerItem[i] = cumulativeSpawnChance;
+        }
+    }
+
+    public string GetRandomItem() {
+        float rand = Random.Range(0f, cumulativeSpawnChance);
+        for (int i = 0; i < spawnChancesPerItem.Length; i++){
+            if (rand <= spawnChancesPerItem[i])
+                return ItemsToSpawn[i].Name;
+        }
+
+        return ItemsToSpawn[ItemsToSpawn.Length].Name;
+    }
+}
