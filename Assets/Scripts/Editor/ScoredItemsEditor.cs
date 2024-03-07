d239eb9fa8dcdbdb71b987862459dae4070aa7bf
Kyra <kyra@notsuspicious.biz>
2024-03-07 12:24:12 -0800

    scored items only reveal "good cheese" property when relevant

--- Assets/Scripts/Editor/ScoredItemsEditor.cs
+++ Assets/Scripts/Editor/ScoredItemsEditor.cs
@@ -1,47 +1,50 @@
 using UnityEngine;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-//IMPORTANT: THIS WAS AN OLD DRAFT, DO NOT USE. KEEPING FOR REFERENCE UNTIL I CAN DELETE
-
-/*[CustomEditor(typeof(ScoreItem))]
-public class ScoredItemsEditor : Editor {
-    public ItemTag itemType;
-    public override void OnInspectorGUI() {
-        serializedObject.Update();
-
-        //ScoreItem item = (ScoreItem)target;
-
-        GUILayout.Label("Test");
-
-        *//*itemType = (ItemTag)EditorGUILayout.EnumPopup("Item", itemType);
-        if (itemType == ItemTag.Cheddar || itemType == ItemTag.Gruyere)
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("isGoodCheese"));
-*//*
-
-        serializedObject.ApplyModifiedProperties();
-    }
-}*/
-
-/*[CustomPropertyDrawer(typeof(ScoreItem))]
+[CustomPropertyDrawer(typeof(ScoreItem))]
 public class ScoredItemsEditor : PropertyDrawer {
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+        SerializedProperty item = property.FindPropertyRelative("item");
+
+        float height = EditorGUIUtility.singleLineHeight;
+        if (property.isExpanded) {
+            height += EditorGUI.GetPropertyHeight(item);
+            ItemTag tagValue = (ItemTag)item.intValue;
+            if (tagValue == ItemTag.Cheddar || tagValue == ItemTag.Gruyere) {
+                SerializedProperty isGoodCheese = property.FindPropertyRelative("isGoodCheese");
+                height += EditorGUI.GetPropertyHeight(isGoodCheese);
+            }
+        }
+
+        return height;
+    }
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
         EditorGUI.BeginProperty(position, label, property);
 
-        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+        Rect foldoutRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+        property.isExpanded = EditorGUI.Foldout(foldoutRect, property.isExpanded, label);
 
-        ScoreItem itemToScore = (ScoreItem)attribute;
+        if (property.isExpanded) {
+            EditorGUI.indentLevel++;
 
-        var enumRect = new Rect(position.x + 20, position.y, position.width - 90, position.height);
-        var boolRect = new Rect(position.x + 90, position.y, position.width - 90, position.height);
-        EditorGUI.PropertyField(enumRect, property.FindPropertyRelative("item"), GUIContent.none);
+            SerializedProperty item = property.FindPropertyRelative("item");
 
-        if (itemToScore != null && (itemToScore.item == ItemTag.Cheddar || itemToScore.item == ItemTag.Gruyere))
-            EditorGUI.PropertyField(boolRect, property.FindPropertyRelative("isCheeseGood"), GUIContent.none);
-        else
-            itemToScore.isGoodCheese = false;
+            Rect rect = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, position.width, EditorGUI.GetPropertyHeight(item));
+            EditorGUI.PropertyField(rect, item);
+
+            ItemTag tagValue = (ItemTag)item.intValue;
+            if (tagValue == ItemTag.Cheddar || tagValue == ItemTag.Gruyere) {
+                SerializedProperty isGoodCheese = property.FindPropertyRelative("isGoodCheese");
+                Rect cheeseRect = new Rect(position.x, position.y + (2 * EditorGUIUtility.singleLineHeight), position.width, EditorGUI.GetPropertyHeight(isGoodCheese));
+                EditorGUI.PropertyField(cheeseRect, isGoodCheese);
+            }
+
+        }
 
         EditorGUI.EndProperty();
     }
-}*/
+}
--- Assets/Scripts/ScoringParameters.cs
+++ Assets/Scripts/ScoringParameters.cs
@@ -1,15 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 [CreateAssetMenu(fileName = "New Scoring Parameters", menuName = "ScriptableObjects/Scoring Parameters SO")]
 public class ScoringParameters : ScriptableObject {
     [System.Serializable] public struct ScoreScenario {
-        public EnumDataExtension<ScoreItem, ItemTag> inputItem;
-        public EnumDataExtension<ScoreItem, ItemTag> desiredItem;
+        public ScoreItem inputItem;
+        public ScoreItem desiredItem;
         public float score;
     }
 
@@ -17,6 +14,9 @@ public class ScoringParameters : ScriptableObject {
 }
 
 [System.Serializable] public class ScoreItem {
-    public bool isGoodCheese;
+    //see the ScoredItemsEditor class for inspector-based changes made here
+
+    public ItemTag item;
+    [SerializeField] private bool isGoodCheese = false;
 }
 
