8433f3a1b9b355108f8c5fcdfcefe8870dae98ef
Kyra <kyra@notsuspicious.biz>
2024-03-12 12:32:52 -0700

    test score values

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -5,13 +5,13 @@ using TMPro;
 
 public class ScoreKeeper : MonoBehaviour
 {
-    public float score;
+    public float Score { get; private set; }
     [SerializeField] TMP_Text scoreText;
     public static ScoreKeeper Instance { get; private set; }
 
     void Awake()
     {
-        score = 0;
+        Score = 0;
         if (Instance != null) {
             Debug.LogWarning($"An earlier instance of ScoreKeeper on {Instance.gameObject.name} was replaced by one on {gameObject.name}.");
             DestroyImmediate(Instance);
@@ -26,13 +26,14 @@ public class ScoreKeeper : MonoBehaviour
                 targetScenario = scenario;
         }
 
+        //if there is no target scenario, assume the score is unchanged
         if (targetScenario == null) {
-            Debug.LogError($"There is no score scenario to compare the input {item.item} to  the desired {desiredItem.item}. Please check the Settings GameObject and make sure all possible score scenarios are present.");
+            Debug.LogWarning($"There is no score scenario to compare the input {item.item} to  the desired {desiredItem.item}. If this is incorrect, please check your Scoring Parameters.");
             return;
         }
 
-        score += targetScenario.score;
-        scoreText.text = "Score: " + score;
+        Score += targetScenario.score;
+        scoreText.text = "Score: " + Score;
 
     }
 
