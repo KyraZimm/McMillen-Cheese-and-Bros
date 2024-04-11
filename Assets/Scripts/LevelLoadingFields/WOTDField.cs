2d88c806c386519f407ffe72dc2d7d0216ba6405
Kyra <klz245@nyu.edu>
2024-04-11 11:08:01 -0700

    wotd updates on level load

--- Assets/Scripts/LevelLoadingFields/WOTDField.cs
+++ Assets/Scripts/LevelLoadingFields/WOTDField.cs
@@ -3,18 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class WOTDField : MonoBehaviour {
-    public static WOTDField Instance { get; private set; }
-
-    private void Awake() {
-        if (Instance != null) {
-            Debug.LogWarning($"An earlier instance of WOTDField on {Instance.gameObject.name} was replaced by one on {gameObject.name}.");
-            DestroyImmediate(Instance);
-        }
-        Instance = this;
-    }
-
-    public void UpdateWOTD() {
-
+public class WOTDField : MonoBehaviour,  ILevelLoadField {
+    [SerializeField] TMP_Text textBox;
+    void ILevelLoadField.OnLevelLoad(LevelValues levelToLoad) {
+        textBox.text = levelToLoad.WordOfTheDay.ToUpper();
     }
 }
