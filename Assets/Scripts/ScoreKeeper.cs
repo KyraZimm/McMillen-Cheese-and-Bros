78f67cd56ebfd0c70308db77d20b19274fc94f62
Kyra <klz245@nyu.edu>
2024-04-11 11:29:04 -0700

    temp hack to scorekeeper to make fields update correctly

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class ScoreKeeper : MonoBehaviour
-{
-    public float Score { get; private set; }
-    [SerializeField] TMP_Text scoreText;
+public class ScoreKeeper : MonoBehaviour {
     public static ScoreKeeper Instance { get; private set; }
+
+    [SerializeField] TMP_Text winsText;
+    [SerializeField] TMP_Text learningExpsText;
+
+    //public float Score { get; private set; }
+    public float Wins { get; private set; }
+    public float LearningExps { get; private set; }
 
     void Awake()
     {
@@ -17,12 +21,21 @@ public class ScoreKeeper : MonoBehaviour
         }
         Instance = this;
 
-        Score = 0;
-        scoreText.text = "Score: " + Score;
+       /* Score = 0;
+        score.text = "Score: " + Score.ToString();*/
+
+        Wins = 0;
+        LearningExps = 0;
+
+        winsText.text = "Wins: " + Wins.ToString();
+        learningExpsText.text = "Learning Experiences: " + LearningExps.ToString();
     }
 
     public void ModifyScore(ScoreItem item, ScoreItem desiredItem) {
-        ScoringParameters.ScoreScenario targetScenario = null;
+
+        //NOTE: this should undergo major refactors later, if it's not reverted at some point
+
+        /*ScoringParameters.ScoreScenario targetScenario = null;
         foreach (ScoringParameters.ScoreScenario scenario in LevelManager.Instance.ScoringParameters.scoreScenarios) {
             if (scenario.inputItem.Match(item) && scenario.desiredItem.Match(desiredItem))
                 targetScenario = scenario;
@@ -30,12 +43,21 @@ public class ScoreKeeper : MonoBehaviour
 
         //if there is no target scenario, assume the score is unchanged
         if (targetScenario == null) {
-            Debug.LogWarning($"There is no score scenario to compare the input {item.item} to  the desired {desiredItem.item}. If this is incorrect, please check your Scoring Parameters.");
+            Debug.LogWarning($"There is no score scenario to compare the input {item.item} to the desired {desiredItem.item}. If this is incorrect, please check your Scoring Parameters.");
             return;
         }
 
         Score += targetScenario.score;
-        scoreText.text = "Score: " + Score;
+        scoreText.text = "Score: " + Score;*/
+
+        if (desiredItem.Match(item)) {
+            Wins++;
+            winsText.text = "Wins: " + Wins.ToString();
+        }
+        else {
+            LearningExps++;
+            learningExpsText.text = "Learning Experiences: " + LearningExps.ToString();
+        }
 
     }
 
