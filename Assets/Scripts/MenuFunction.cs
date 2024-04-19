9e879ee214c18bdfd723ed74cb75b6ad203757ed
Kyra <klz245@nyu.edu>
2024-04-19 11:43:02 -0700

    menu function updates

--- Assets/Scripts/MenuFunction.cs
+++ Assets/Scripts/MenuFunction.cs
@@ -34,21 +34,33 @@ public class MenuFunction : MonoBehaviour
     // Update is called once per frame
     public void CheckCode()
     {
+        playerInput = playerInputField.text;
+
         Debug.Log(playerInput); //This is not returning the text I put into the game. I assume it's because I'm not pulling it properly
 
-        bool matchExists = false;
+        LevelValues level = LevelReference.Instance.GetDay(playerInput); //this basically does what your code below was doing. If a level exists, it'll give you the LevelValues that match the word of the day. If not, it'll be null (empty)
+        if (level != null) { //if level == null, no level matched the word of the day. if level != null, it means a match was found
+            levelStart.SetActive(true);
+        }
+        else {
+            qualityCheck.SetActive(true);
+        }
+
+        //Lydia's code:
+
+        /*bool matchExists = false;
         for (int i = 0; i < LevelReference.Instance.Levels.Length; i++)
         {
            if (LevelReference.Instance.Levels[i].WordOfTheDay == playerInput)
-            {
+           {
                 matchExists = true;
                 levelStart.SetActive(true);
-            }
+           }
         }
 
         if (!matchExists)
         {
             qualityCheck.SetActive(true);
-        }
+        }*/
     }
 }
